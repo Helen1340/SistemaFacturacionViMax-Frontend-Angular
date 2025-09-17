4865e55b036bd9c2df4df2ef86e5b044185d3398
Helen <helendiaz.1340@gamil.com>
2025-09-16 20:35:32 -0500

    feature: ajustes en módulo productos y registro de servicio-producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProductosServicios } from './module-product-service/productos-servicios/productos-servicios';
-import { RegistrarProducto } from './module-product-service/registrar-producto/registrar-producto';
+
 import { RegitrarServicio } from './module-product-service/regitrar-servicio/regitrar-servicio';
 import { Clientes } from './module-clients/clientes/clientes';
 import { Reportes } from './module-reports/reportes/reportes';
@@ -10,6 +10,7 @@ import { RecepcionDocumentos } from './module-document-reception/recepcion-docum
 import { Notificaciones } from './module-notifications/notificaciones/notificaciones';
 import { Configuracion } from './module-settings/configuracion/configuracion';
 import { FacturasNotas } from './module-invoices-notes/facturas-notas/facturas-notas';
+import { RegistrarProductoComponent } from './module-product-service/registrar-producto/registrar-producto';
 
 
 
@@ -93,7 +94,7 @@ export const routes: Routes = [
 
   //ESPACIO PARA LAS RUTAS DE HELEN (MODULE PRODUCTOS)
   { path: 'productos-servicios', component: ProductosServicios},
-  {path: 'registrar-producto', component: RegistrarProducto},
+  {path: 'registrar-producto', component: RegistrarProductoComponent},
   {path: 'registrar-servicio',component: RegitrarServicio},
   
   // ... otras rutas
@@ -191,6 +192,20 @@ export const routes: Routes = [
 
 //ESPACIO PARA LAS RUTAS DE (MODULE COFIGURACIONES)
 {path: 'configuración',component:Configuracion},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ];
 
