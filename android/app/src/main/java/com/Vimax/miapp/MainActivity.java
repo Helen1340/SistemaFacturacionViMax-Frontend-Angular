55487f6ba7cba4d3bf0bd2569657f5a8f912ca28
Pw-Wilfran <wilfranpilcue@gmail.com>
2025-11-25 23:07:29 -0500

    arreglos responsive

--- android/app/src/main/java/com/Vimax/miapp/MainActivity.java
+++ android/app/src/main/java/com/Vimax/miapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.Vimax.miapp;
 
+import android.app.DownloadManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.Window;
+import android.webkit.DownloadListener;
 import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
